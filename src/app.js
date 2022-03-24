ad432a3f356263e9a1165a585444023f383e1dcc
Jan Kalbermatter <jan.kalbermatter@gmail.com>
2022-03-24 14:02:33 +0100

    Log router, return json

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const app = express()
 
+// app.use(allowCrossDomain)
+
 app.get("/test", (_req, res) =>  {
   res.status(200).send("{}")
 })
 
 app.get("/api/log", (_req, res) =>  {
-  console.log(_req, res)
-  res.status(200).send(JSON.stringify({"req": _req.rawHeaders}))
+  res.status(200).send(JSON.stringify({"Success": true}))
 })
 module.exports = app;
