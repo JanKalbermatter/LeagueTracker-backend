372edc1cd5523dc3e389ec4adc3623e228349f48
Jan Kalbermatter <jan.kalbermatter@gmail.com>
2022-03-24 16:25:57 +0100

    no crash without db, log atlas_url (testing)

--- src/server/routes/record.js
+++ src/server/routes/record.js
@@ -6,21 +6,24 @@ const dbo = require("../../db/conn");
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /listings.
 const recordRoutes = express.Router();
+console.log(process.env.ATLAS_URI);
 
 // This section will help you get a list of all the documents.
 recordRoutes.route("/user").get(async function (req, res) {
     const dbConnect = dbo.getDb();
 
-    dbConnect
-        .collection("user")
-        .find({}).limit(50)
-        .toArray(function (err, result) {
-        if (err) {
-            res.status(400).send("Error fetching listings!");
-        } else {
-            res.json(result);
-        }
-    });
+    if(dbConnect) {
+        dbConnect
+            .collection("user")
+            .find({}).limit(50)
+            .toArray(function (err, result) {
+            if (err) {
+                res.status(400).send("Error fetching listings!");
+            } else {
+                res.json(result);
+            }
+        });
+    }
 });
 
 // This section will help you get a list of all the documents.
