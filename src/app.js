3090ec31cbd6517040ec4e974819829be55f6f8f
Jan Kalbermatter <jan.kalbermatter@gmail.com>
2022-03-24 13:44:14 +0100

    return for log-route

--- src/app.js
+++ src/app.js
@@ -4,4 +4,9 @@ const app = express()
 app.get("/test", (_req, res) =>  {
   res.status(200).send("{}")
 })
+
+app.get("/api/log", (_req, res) =>  {
+  console.log(_req, res)
+  res.status(200).send(JSON.stringify({"req": _req.rawHeaders}))
+})
 module.exports = app;
