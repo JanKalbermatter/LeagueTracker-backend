e52ffeafb405b740f0bdf1b5fe8e1dd9c5d6a507
Jan Kalbermatter <jan.kalbermatter@gmail.com>
2022-03-24 15:56:19 +0100

    rework structure, changed githubAction

---
+++ src/server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const cors = require("cors");
+const record = require("./routes/record")
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(record);
+
+// get driver connection
+const dbo = require("../db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
+
+module.exports = app
