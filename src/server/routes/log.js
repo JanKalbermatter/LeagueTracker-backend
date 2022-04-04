ee7dbc6edf8b6fb749ceb5a0c8c56decbc21c0cc
Jan Kalbermatter <jan.kalbermatter@gmail.com>
2022-04-04 16:08:18 +0200

    Put route, multiple get routes for logs

--- src/server/routes/log.js
+++ src/server/routes/log.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
+
 // get driver connection
 const dbo = require("../../db/conn");
 
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /listings.
 const logRoutes = express.Router();
 
-// return a list with the logs
+// return a list with the logEntries
 logRoutes.route("/api/v1/log").get(async function (req, res) {
     const dbConnect = dbo.getDb();
 
@@ -15,16 +14,74 @@ logRoutes.route("/api/v1/log").get(async function (req, res) {
         dbConnect
             .collection("log")
             .find({})
-            .toArray(function (err, result) {
+            .toArray((err, result) => returnFetchedResults(res, err, result));
+    } else {
+        res.json({Msg: "No DB Connection"});
+    }
+});
+
+// return the requested logEntry (logId)
+logRoutes.route("/api/v1/log/:logId").get(async function (req, res) {
+    const dbConnect = dbo.getDb();
+    const logId = req.params.logId
+
+    if(dbConnect) {
+        dbConnect
+            .collection("log")
+            .find({ _id: ObjectId(logId)})
+            .toArray((err, result) => returnFetchedResults(res, err, result));
+    } else {
+        res.json({Msg: "No DB Connection"});
+    }
+});
+
+// return the requested logEntry (timestamp from-until)
+logRoutes.route("/api/v1/log/date/:start/:end").get(async function (req, res) {
+    const dbConnect = dbo.getDb();
+
+    const start = req.params.start
+    const end = req.params.end
+
+    if(dbConnect) {
+        dbConnect
+            .collection("log")
+            .find({ entryDate: {$gte: start, $lte: end}})
+            .toArray((err, result) => returnFetchedResults(res, err, result));
+    } else {
+        res.json({Msg: "No DB Connection"});
+    }
+});
+
+// Add a new logEntry
+logRoutes.route("/api/v1/log").put(async function (req, res) {
+    const dbConnect = dbo.getDb();
+    const logEntry = req.body
+
+    if(dbConnect) {
+        // Insert LogEntry to collection log
+        dbConnect.collection("log").insertOne(logEntry, (err, result) => {
             if (err) {
-                res.status(400).send("Error fetching listings!");
+                res.status(400).send({ Msg: "Entry not created."});
             } else {
-                res.json(result);
+                res.status(200).json(result);
             }
-        });
+        })
     } else {
         res.json({Msg: "No DB Connection"});
     }
 });
 
 module.exports = logRoutes
+
+// Return the results from mongoDB. Show error if request failed. 
+function returnFetchedResults(result, error, returnValue=null, errorMessage='Error fetching entries!') {
+    if(!returnValue) {
+        returnValue = result
+    }
+
+    if (error) {
+        result.status(400).send(errorMessage);
+    } else {
+        result.json(returnValue);
+    }
+}
--- src/server/routes/record.js
+++ src/server/routes/record.js
@@ -1,20 +1,18 @@
 const express = require("express");
+
 // get driver connection
 const dbo = require("../../db/conn");
 
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /listings.
 const recordRoutes = express.Router();
 
-// return a list with the first 50 users
+// return a list with the users
 recordRoutes.route("/api/v1/user").get(async function (req, res) {
     const dbConnect = dbo.getDb();
 
     if(dbConnect) {
         dbConnect
             .collection("user")
-            .find({}).limit(50)
+            .find({})
             .toArray(function (err, result) {
             if (err) {
                 res.status(400).send("Error fetching listings!");
